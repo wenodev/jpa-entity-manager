80bcde01fb8df76728c05d70208a283b3e764c44
weno <35131529+wenodev@users.noreply.github.com>
2024-11-13 00:57:12 +0900

    feat: EntityEntry를 사용하도록 변경

--- src/main/java/persistence/sql/entity/PersistenceContext.java
+++ src/main/java/persistence/sql/entity/PersistenceContext.java
@@ -4,43 +4,61 @@ import jakarta.persistence.Entity;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 class PersistenceContext {
     private final Map<CacheKey, CacheEntry> entityCache = new HashMap<>();
+    private final Map<CacheKey, EntityEntry> entityEntries = new HashMap<>();
 
-    Optional<Object> find(final Class<?> entityClass, final Long id) {
-        return Optional.ofNullable(id)
-                .map(validId -> getCacheKey(entityClass, validId))
-                .map(entityCache::get)
+    Optional<Object> getEntity(final CacheKey key) {
+        return Optional.ofNullable(entityCache.get(key))
                 .map(CacheEntry::getEntity);
     }
 
-    void put(final Class<?> entityClass, final Long id, final Object entity) {
-        Optional.ofNullable(id)
-                .map(validId -> getCacheKey(entityClass, validId))
-                .ifPresent(key -> {
-                    entityCache.put(key, new CacheEntry(entity));
-                });
+    void addEntity(final CacheKey key, final Object entity) {
+        final CacheEntry cacheEntry = new CacheEntry(entity);
+        entityCache.put(key, cacheEntry);
+        entityEntries.put(key, new EntityEntry(Status.NEW));
     }
 
-    void remove(final Class<?> entityClass, final Long id) {
-        Optional.ofNullable(id)
-                .map(validId -> getCacheKey(entityClass, validId))
-                .ifPresent(entityCache::remove);
+    void managedEntity(final CacheKey key, final Object entity) {
+        final CacheEntry cacheEntry = new CacheEntry(entity);
+        entityCache.put(key, cacheEntry);
+        entityEntries.put(key, new EntityEntry(Status.MANAGED));
+    }
+
+    void removeEntity(final CacheKey key) {
+        if (entityEntries.containsKey(key)) {
+            final EntityEntry entry = entityEntries.get(key);
+            entry.updateStatus(Status.DELETED);
+            entityCache.remove(key);
+        }
+    }
+
+    boolean containsEntity(final Class<?> entityClass, final Long id) {
+        final CacheKey key = getCacheKey(entityClass, id);
+        return entityCache.containsKey(key);
     }
 
     Map<CacheKey, Object> getDirtyEntities() {
         return entityCache.entrySet().stream()
-                .filter(entry -> entry.getValue().isDirty())
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        entry -> entry.getValue().getEntity()
-                ));
+                .filter(this::isDirtyAndManagedEntity)
+                .collect(toCacheKeyObjectMap());
     }
 
-    void clearDirtyEntities() {
-        entityCache.values().forEach(CacheEntry::clearDirty);
+    private Collector<Map.Entry<CacheKey, CacheEntry>, ?, Map<CacheKey, Object>> toCacheKeyObjectMap() {
+        return Collectors.toMap(
+                Map.Entry::getKey,
+                entry -> entry.getValue().getEntity());
+    }
+
+    private boolean isDirtyAndManagedEntity(final Map.Entry<CacheKey, CacheEntry> entry) {
+        final CacheKey key = entry.getKey();
+        final EntityEntry entityEntry = entityEntries.get(key);
+        return entry.getValue().isDirty() &&
+               entityEntry != null &&
+               entityEntry.isManaged();
     }
 
     private CacheKey getCacheKey(final Class<?> entityClass, final Long id) {
@@ -49,9 +67,8 @@ class PersistenceContext {
     }
 
     private void validateEntity(final Class<?> entityClass) {
-        if (!entityClass.isAnnotationPresent(Entity.class))
-            throw new IllegalArgumentException(
-                    String.format("Class %s must be annotated with @Entity", entityClass.getSimpleName())
-            );
+        if (!entityClass.isAnnotationPresent(Entity.class)) {
+            throw new IllegalArgumentException("Class must be annotated with @Entity");
+        }
     }
 }
